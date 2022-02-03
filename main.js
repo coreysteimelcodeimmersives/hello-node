ddbaad1c9742af975e43bcc60ac5881d0e55a340
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-02-03 13:51:55 -0700

    Finished

---
+++ main.js
@@ -0,0 +1,42 @@
+console.log("\n");
+
+console.log("hello world");
+
+console.log("\n");
+
+console.log("HH   HH        lll lll                                  lll      dd !!!\n"+
+"HH   HH   eee  lll lll  oooo   ww      ww  oooo  rr rr  lll      dd !!!\n"+
+"HHHHHHH ee   e lll lll oo  oo  ww      ww oo  oo rrr  r lll  dddddd !!!\n"+
+"HH   HH eeeee  lll lll oo  oo   ww ww ww  oo  oo rr     lll dd   dd\n"+
+"HH   HH  eeeee lll lll  oooo     ww  ww    oooo  rr     lll  dddddd !!!");
+
+console.log("\n");
+
+console.log(" ____    _             _                   _   \n"+
+"| __ )  (_)   __ _    | |_    ___  __  __ | |_ \n"+
+"|  _ \\  | |  / _` |   | __|  / _ \\ \\ \\/ / | __|\n"+
+"| |_) | | | | (_| |   | |_  |  __/  >  <  | |_ \n"+
+"|____/  |_|  \\__, |    \\__|  \\___| /_/\\_\\  \\__|\n"+
+"             |___/                             ");
+
+console.log("\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
